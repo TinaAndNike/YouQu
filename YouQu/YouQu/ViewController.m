aea4ffd022c60776d57565b014e04500d23723ac
夏梦雷 <490442541@qq.com>
2017-05-31 10:02:51 +0800

    5_31,创建项目

---
+++ YouQu/YouQu/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YouQu
+//
+//  Created by 夏梦雷 on 17/5/31.
+//  Copyright © 2017年 夏梦雷. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ YouQu/YouQu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YouQu
+//
+//  Created by 夏梦雷 on 17/5/31.
+//  Copyright © 2017年 夏梦雷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
